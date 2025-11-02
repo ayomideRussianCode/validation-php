865072734d6a56fe5247c4d89d3d90808a8c2965
ayomideRussianCode <ayindeayomide7@gmail.com>
2025-11-02 08:01:06 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Validation</title>
+</head>
+<body>
+    <form action="process.php" method="post">
+        <input name="username" placeholder="Enter Username" type="text">
+        <input name="email" placeholder="Enter Email" type="text"><br><br>
+        <input type="submit">
+    </form>
+</body>
+</html>
